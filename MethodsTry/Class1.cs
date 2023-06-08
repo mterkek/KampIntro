34612facb3efa67ca960a4150bfaac8b8f7d8105
mterkek <m.t.erkek@hotmail.com>
2023-06-08 18:22:42 +0300

    class method demo

---
+++ MethodsTry/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsTry
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("added");
+        }
+    }
+   
+}
---
+++ MethodsTry/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodsTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("added!");
+        }
+        static void Add2(int num1, int num2)
+        {
+            Console.WriteLine("added!");
+        }
+    }
+}
