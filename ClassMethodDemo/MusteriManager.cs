5fecdeb11d684b8005fefce0bab7279b671cc315
mterkek <m.t.erkek@hotmail.com>
2023-06-08 18:22:20 +0300

    ClassMetotDemo

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " müşteri eklendi.");
+        }
+        public void Listed(Customer customer)
+        {
+            Console.WriteLine("Müşteri No: " + customer.Id);
+            Console.WriteLine("Müşteri Adı: " + customer.Name);
+            Console.WriteLine("Müşteri Soyadı: " + customer.Surname);
+            Console.WriteLine("Müşteri Adresi: " + customer.Address);
+
+            Console.WriteLine("-----------------------------------");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine($"{customer.Id} no'lu müşteri silindi.");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassMethotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Mustafa Taha";
+            customer1.Surname = "ERKEK";
+            customer1.Address = "Elazığ";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Engin";
+            customer2.Surname = "Demiroğ";
+            customer2.Address = "Diyarbakır";
+
+            // Customer[] customers = new Customer[] { customer1, customer2 };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            customerManager.Listed(customer1);
+            customerManager.Listed(customer2);
+
+            customerManager.Delete(customer1);
+
+        }
+    }
+}
