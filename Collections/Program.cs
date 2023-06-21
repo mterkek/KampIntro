20a36594feb8be18874b3154214f284229206c95
mterkek <m.t.erkek@hotmail.com>
2023-06-21 18:33:56 +0300

    productmanage

---
+++ Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ctrl k ctrl c command satırı yapar seçili satırları geri almak için ctrl k ctrl u
+            //string[] names = new string[] {"mustafa","burak","ertuğrul"};
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+
+            List<string> names2 = new List<string> { "taha", "furkan", "faruk" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            names2.Add("ayhan");
+            Console.WriteLine(names2[3]);
+            Console.WriteLine(names2[1]);
+
+        }
+    }
+}
